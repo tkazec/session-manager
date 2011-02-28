tkazec/session-manager

commit 81b475e3fbef78ec804df1d750046091699cbc62

Added omnibox functionality
 - Added ability to open sessions with the omnibox.
 - Added omnibox help to the options page.
 - Reorganized background.js.
 - Removed ability to save sessions with a zero character name, because besides the UI issues it causes, it could have other unknown side effects.

js/background.js
@@ -1,5 +1,6 @@
-(function(localStorage){
+(function(chrome, localStorage){
 
+/*** setup ***/
 var cversion = "3.0.0";
 
 localStorage.sessions = localStorage.sessions || '{}';
@@ -31,11 +32,54 @@ if (localStorage.version === cversion) {
 	localStorage.version = cversion;
 }
 
-})(localStorage);
 
-function openSession(cwinId, urls, e) {
-	var open = JSON.parse(localStorage.open),
-		action = typeof e === "string" ? e : (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click");
+/*** omnibox ***/
+function defaultSuggestion() {
+	chrome.omnibox.setDefaultSuggestion({ description: "Open a session in this window" });
+}
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest){
+	var sessions = JSON.parse(localStorage.sessions), text = text.trim(), ltext = text.toLowerCase(), suggestions = [];
+	
+	if (text.length) {
+		chrome.omnibox.setDefaultSuggestion({ description: "Open <match>" + text + "</match>" + (sessions[text] ? "" : " ...") + " in this window" });
+		
+		Object.keys(sessions).forEach(function(name){
+			var index = name.toLowerCase().indexOf(ltext);
+			
+			index !== -1 && suggestions.push({
+				content: name,
+				description: name.substring(0, index) + "<match>" + name.substr(index, text.length) + "</match>" + name.substr(index + text.length),
+				index: index
+			});
+		});
+		
+		suggest(suggestions.sort(function(a, b){
+			return a.index === b.index ? (a.content.length === b.content.length ? 0 : a.content.length - b.content.length) : a.index - b.index;
+		}));
+	} else {
+		defaultSuggestion();
+	}
+});
+
+chrome.omnibox.onInputEntered.addListener(function(name){
+	var sessions = JSON.parse(localStorage.sessions);
+	
+	sessions[name] && chrome.windows.getCurrent(function(win){
+		chrome.tabs.getSelected(win.id, function(tab){
+			openSession(win.id, sessions[name], {});
+			
+			chrome.tabs.remove(tab.id);
+		});
+	});
+});
+
+defaultSuggestion();
+
+
+/*** open ***/
+this.openSession = function(cwinId, urls, e){
+	var open = JSON.parse(localStorage.open), action = (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click");
 	
 	if (action === open["add"]) {
 		urls.forEach(function(v){
@@ -43,14 +87,17 @@ function openSession(cwinId, urls, e) {
 		});
 	} else if (action === open["replace"]) {
 		chrome.tabs.getAllInWindow(cwinId, function(tabs){
-			openSession(cwinId, urls, open.add);
+			openSession(cwinId, urls, {});
+			
 			tabs.forEach(function(tab){
 				chrome.tabs.remove(tab.id);
 			});
 		});
 	} else if (action === open["new"] || action === open["incognito"]) {
 		chrome.windows.create({ url: urls.shift(), incognito: action === open["incognito"] }, function(win){
-			openSession(win.id, urls, open.add);
+			openSession(win.id, urls, {});
 		});
 	}
 }
+
+})(chrome, localStorage);

options.html
@@ -46,6 +46,9 @@
 				<li>Click "Remove" to remove the session. You will be asked to confirm the removal.</li>
 			</ul>
 			
+			<h2>Omnibox</h2>
+			<p>Sessions can be opened by typing "session" in the omnibox and pressing the tab or space key, then entering the session name and pressing enter. Session Manager may suggest some sessions based on the text entered.</p>
+			
 			<h2>Incognito</h2>
 			<p>"Allow in incognito" must be checked under Session Manager in chrome://extensions for the incognito features to work. Sessions can be saved and opened normally in existing incognito windows, and can be opened in new incognito windows with the combo in the "Opening sessions" options.</p>
 			

js/popup.js
@@ -107,10 +107,13 @@ var actions = {
 	}, function(oname){
 		var nname = state.entered = $("#rename-text").val().trim();
 		
-		if (sessions.list[nname]) {
-			utils.confirm("Are you sure you want to replace " + sessions.display(nname) + " by renaming " + sessions.display(oname) + "?", 2);
-		} else {
-			utils.action("rename", 2);
+		if (nname) {
+			if (sessions.list[nname]) {
+				utils.confirm("Are you sure you want to replace " + sessions.display(nname) + " by renaming " + sessions.display(oname) + "?", 2);
+			} else {
+				utils.action("rename", 2);
+				utils.view("main");
+			}
 		}
 	}, function(oname){
 		sessions.list[state.entered] = sessions.list[oname];
@@ -156,9 +159,12 @@ var actions = {
 	
 	save: [function(){
 		var $name = $("#main-save-name"), name = state.name = $name.val().trim();
-		$name.val("");
 		
-		utils.action("replace", sessions.list[name] ? 2 : 1);
+		if (name) {
+			$name.val("");
+			
+			utils.action("replace", sessions.list[name] ? 2 : 1);
+		}
 	}]
 };
 

popup.html
@@ -41,8 +41,8 @@
 		
 		<fieldset id="rename"><legend id="rename-legend"></legend>
 			<div class="box">
-				<input type="text" id="rename-text" class="flex" data-view="main" data-action="" data-actionindex="1">
-				<button data-view="main" data-action="" data-actionindex="1">Rename</button>
+				<input type="text" id="rename-text" class="flex" data-action="" data-actionindex="1">
+				<button data-action="" data-actionindex="1">Rename</button>
 				<button data-view="main">Cancel</button>
 			</div>
 		</fieldset>

commit e121eddf1a14ae0a700eb1fd367c394b1a34c71e

Fixed severe bug
Fixed a severe bug introduced in 81b475e3fbef78ec804df1d750046091699cbc62.

js/background.js
@@ -1,7 +1,7 @@
 (function(chrome, localStorage){
 
 /*** setup ***/
-var cversion = "3.1.0";
+var cversion = "3.1.1";
 
 localStorage.sessions = localStorage.sessions || '{}';
 localStorage.open = localStorage.open || '{"add":"click", "replace":"shift+click", "new":"ctrl/cmd+click", "incognito":"alt+click"}';
@@ -67,7 +67,7 @@ chrome.omnibox.onInputEntered.addListener(function(name){
 	
 	sessions[name] && chrome.windows.getCurrent(function(win){
 		chrome.tabs.getSelected(win.id, function(tab){
-			openSession(win.id, sessions[name], {});
+			openSession(win.id, sessions[name]);
 			
 			chrome.tabs.remove(tab.id);
 		});
@@ -79,7 +79,7 @@ defaultSuggestion();
 
 /*** open ***/
 this.openSession = function(cwinId, urls, e){
-	var open = JSON.parse(localStorage.open), action = (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click");
+	var open = JSON.parse(localStorage.open), action = e == null ? open["add"] : (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click");
 	
 	if (action === open["add"]) {
 		urls.forEach(function(v){
@@ -87,7 +87,7 @@ this.openSession = function(cwinId, urls, e){
 		});
 	} else if (action === open["replace"]) {
 		chrome.tabs.getAllInWindow(cwinId, function(tabs){
-			openSession(cwinId, urls, {});
+			openSession(cwinId, urls);
 			
 			tabs.forEach(function(tab){
 				chrome.tabs.remove(tab.id);
@@ -95,7 +95,7 @@ this.openSession = function(cwinId, urls, e){
 		});
 	} else if (action === open["new"] || action === open["incognito"]) {
 		chrome.windows.create({ url: urls.shift(), incognito: action === open["incognito"] }, function(win){
-			openSession(win.id, urls, {});
+			openSession(win.id, urls);
 		});
 	}
 }

options.html
@@ -59,6 +59,11 @@
 		<section id="changelog">
 			<h1>Changelog</h1>
 			
+			<h2>3.1.1 (2011-02-28)</h2>
+			<ul>
+				<li>Fixed a severe bug where changing the "add to current window" combo to something other than "click" broke opening sessions.</li>
+			</ul>
+			
 			<h2>3.1.0 (2011-02-24)</h2>
 			<ul>
 				<li>Added omnibox functionality.</li>