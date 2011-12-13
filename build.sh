a409323cf8c59031bd0cdb36b304ffcd63052511
Teddy Cross <tkazec@gmail.com>
2011-12-13 03:44:03 -0500

    added a little to the build script and help

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ rm -rf $OUTDIR $OUTZIP
 cp -R $SRCDIR $OUTDIR
 cd $OUTDIR
 
+rm build.sh
+rm -rf .git
+
 sed -i '' -e "s/##GA##/$GA/" js/background.js
 
 find . -path '*/.*' -prune -o -type f -print | zip $OUTZIP -@
