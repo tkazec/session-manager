35bd40dbf771d85933b6084b472074721647943a
Teddy Cross <tkazec@gmail.com>
2013-01-27 13:23:35 -0800

    upgrade manifest, remove version check, set min version to 20, update code style, v3.4.4

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 var _gaq = _gaq || [];
 
-(function(){ "use strict";
+(function () {
 
 /*** setup ***/
-var version = "3.4.3";
+var version = "3.4.4";
 
 localStorage.sessions = localStorage.sessions || '{}';
 localStorage.open = localStorage.open || '{"add":"click", "replace":"shift+click", "new":"ctrl/cmd+click", "incognito":"alt+click"}';
@@ -18,7 +18,7 @@ _gaq.push(
 
 if (localStorage.version === version) {
 	if ("temp" in localStorage) {
-		JSON.parse(localStorage.temp).forEach(function(v){
+		JSON.parse(localStorage.temp).forEach(function (v) {
 			chrome.tabs.create({ url: v });
 		});
 		
@@ -31,26 +31,26 @@ if (localStorage.version === version) {
 	localStorage.version = version;
 }
 
-var browser = navigator.userAgent.match(/Chrome\/(\d\d?\.\d\d?)/);
-if (browser && browser[1] && browser[1] < 18) {
-	if (isNaN(localStorage.outdated) || Date.now() - localStorage.outdated > 1000 * 60 * 60 * 24 * 7) {
-		localStorage.outdated = true;
-	}
-} else {
-	delete localStorage.outdated;
-}
+document.body.appendChild(function () {
+	var el = document.createElement("script");
+	el.src = "https://ssl.google-analytics.com/ga.js";
+	return el;
+}());
 
 
 /*** omnibox ***/
-chrome.omnibox.onInputChanged.addListener(function(text, suggest){
-	var sessions = JSON.parse(localStorage.sessions), text = text.trim(), ltext = text.toLowerCase(), suggestions = [];
+chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+	var sessions = JSON.parse(localStorage.sessions);
+	text = text.trim();
+	var ltext = text.toLowerCase();
+	var suggestions = [];
 	
 	if (text.length) {
 		chrome.omnibox.setDefaultSuggestion({
 			description: "Open <match>" + text + "</match>" + (sessions[text] ? "" : " ...") + " in this window"
 		});
 		
-		Object.keys(sessions).forEach(function(name){
+		Object.keys(sessions).forEach(function (name) {
 			var index = name.toLowerCase().indexOf(ltext);
 			
 			index !== -1 && suggestions.push({
@@ -60,7 +60,7 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest){
 			});
 		});
 		
-		suggest(suggestions.sort(function(a, b){
+		suggest(suggestions.sort(function (a, b) {
 			return a.index === b.index ? (a.content.length === b.content.length ? 0 : a.content.length - b.content.length) : a.index - b.index;
 		}));
 	} else {
@@ -68,7 +68,7 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest){
 	}
 });
 
-chrome.omnibox.onInputEntered.addListener(function(name){
+chrome.omnibox.onInputEntered.addListener(function (name) {
 	var sessions = JSON.parse(localStorage.sessions);
 	
 	if (sessions[name]) {
@@ -82,9 +82,9 @@ chrome.omnibox.setDefaultSuggestion({ description: "Open a session in this windo
 
 
 /*** open ***/
-window.openSession = function(cwinId, urls, e, isTemp){
-	var open = JSON.parse(localStorage.open),
-		action = e ? (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click") : open.add;
+window.openSession = function (cwinId, urls, e, isTemp) {
+	var open = JSON.parse(localStorage.open);
+	var action = e ? (((e.ctrlKey || e.metaKey) && "ctrl/cmd+click") || (e.shiftKey && "shift+click") || (e.altKey && "alt+click") || "click") : open.add;
 	
 	for (var k in open) {
 		if (action === open[k]) {
@@ -94,23 +94,23 @@ window.openSession = function(cwinId, urls, e, isTemp){
 	}
 	
 	if (action === "add") {
-		urls.forEach(function(v){
+		urls.forEach(function (v) {
 			chrome.tabs.create({ windowId: cwinId, url: v });
 		});
 	} else if (action === "replace") {
-		chrome.tabs.getAllInWindow(cwinId, function(tabs){
+		chrome.tabs.getAllInWindow(cwinId, function (tabs) {
 			openSession(cwinId, urls);
 			
 			if (localStorage.noreplacingpinned) {
-				tabs = tabs.filter(function(t){ return !t.pinned; });
+				tabs = tabs.filter(function (t) { return !t.pinned; });
 			}
 			
-			tabs.forEach(function(tab){
+			tabs.forEach(function (tab) {
 				chrome.tabs.remove(tab.id);
 			});
 		});
 	} else if (action === "new" || action === "incognito") {
-		chrome.windows.create({ url: urls.shift(), incognito: action === "incognito" }, function(win){
+		chrome.windows.create({ url: urls.shift(), incognito: action === "incognito" }, function (win) {
 			openSession(win.id, urls);
 		});
 	} else {
